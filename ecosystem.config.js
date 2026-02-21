caa053d7cf1f9b361e5e4ebc2002306c5655fa49
SmaylovSerikbay <36734648+SmaylovSerikbay@users.noreply.github.com>
2026-02-21 17:42:51 +0500

    Update ecosystem configuration and Nginx settings to improve application deployment. Set working directory for the Node.js application and adjust server name and proxy settings for better routing.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'avrgroup',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
+      cwd: '/var/www/avrgroup',
       env: {
         PORT: 3000,
         NODE_ENV: 'production',
